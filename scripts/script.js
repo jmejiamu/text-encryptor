d27bc96ad65b9f8c62bde926e9377af316a5a54d
jmejiamu <jmejiamu@gmail.com>
2024-07-03 16:54:09 -0700

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+function encryptText() {
+  const inputText = document.getElementById("inputText").value;
+  const shift = 3; // Number of positions to shift the characters
+  let encryptedText = "";
+
+  for (let i = 0; i < inputText.length; i++) {
+    let charCode = inputText.charCodeAt(i);
+
+    // Encrypt uppercase letters
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = ((charCode - 65 + shift) % 26) + 65;
+    }
+    // Encrypt lowercase letters
+    else if (charCode >= 97 && charCode <= 122) {
+      charCode = ((charCode - 97 + shift) % 26) + 97;
+    }
+
+    encryptedText += String.fromCharCode(charCode);
+  }
+
+  document.getElementById("outputText").innerText = encryptedText;
+
+  // remove the style from the output text  aligned to the center
+  const element = document.querySelector(".presentation__output");
+  element.style.display = "block";
+  document.getElementById("hiddenButton").innerHTML = `
+  <button class="presentation__output__hidden__button" onclick="">
+          Copy
+        </button>
+  `;
+}
+
+function decryptText() {
+  const inputText = document.getElementById("outputText").innerText;
+  const shift = 3; // Number of positions to shift the characters back
+  let decryptedText = "";
+
+  for (let i = 0; i < inputText.length; i++) {
+    let charCode = inputText.charCodeAt(i);
+
+    // Decrypt uppercase letters
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = ((charCode - 65 - shift + 26) % 26) + 65;
+    }
+    // Decrypt lowercase letters
+    else if (charCode >= 97 && charCode <= 122) {
+      charCode = ((charCode - 97 - shift + 26) % 26) + 97;
+    }
+
+    decryptedText += String.fromCharCode(charCode);
+  }
+
+  document.getElementById("outputText").innerText = decryptedText;
+}
